0da4b5d350fe2e92b844fb9ce235bb465a30bf66
Ishank Gupta <ishank_18@yahoo.com>
2016-11-15 00:47:53 +0530

    Fixed solr for top nav bar

---
+++ app/helpers/spree/products_helper_decorator.rb
@@ -0,0 +1,13 @@
+module Spree
+  ProductsHelper.class_eval do
+
+    def cache_key_for_products
+      count = @products.count
+      max_updated_at = @products.collect(&:updated_at).max.to_s(:number)
+      # max_updated_at = (@products.maximum(:updated_at) || Date.today).to_s(:number)
+      products_cache_keys = "spree/products/all-#{params[:page]}-#{max_updated_at}-#{count}"
+      (common_product_cache_keys + [products_cache_keys]).compact.join("/")
+    end
+
+  end
+end
