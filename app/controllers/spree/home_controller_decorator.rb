3955742f381b3cfbd9681b4a8fc7a6efd3186fcc
brijesh varshney <brijesh.varshney@w3villa.com>
2016-11-15 19:10:55 +0530

    changed design home page

---
+++ app/controllers/spree/home_controller_decorator.rb
@@ -0,0 +1,29 @@
+module Spree
+  HomeController.class_eval do
+
+    def index
+      @searcher = build_searcher(params.merge(include_images: true))
+      @taxonomies = Spree::Taxonomy.includes(root: :children)
+      # @products = @searcher.retrieve_products.includes(:possible_promotions)
+
+
+
+
+
+       @search = Sunspot.search(Spree::Product) do
+	    	paginate(:page => params[:page] || 1, :per_page => Spree::Config[:products_per_page])
+	    	unless params[:keywords].blank?
+	    		any do
+            fulltext "*#{params[:keywords]}*"
+            fulltext params[:keywords]
+          end
+	    	end
+	    	unless params[:taxon].blank?
+	    		with(:taxon_ids, [params[:taxon]])
+	    	end
+	    end
+	    @products = @search.results
+	  
+   end
+  end
+end
--- app/helpers/spree/products_helper_decorator.rb
+++ app/helpers/spree/products_helper_decorator.rb
@@ -3,7 +3,9 @@ module Spree
 
     def cache_key_for_products
       count = @products.count
-      max_updated_at = @products.collect(&:updated_at).max.to_s(:number)
+       max_updated_at = @products.collect(&:updated_at).max.to_s
+
+      # max_updated_at = @products.collect(&:updated_at).max.to_s(:number)
       # max_updated_at = (@products.maximum(:updated_at) || Date.today).to_s(:number)
       products_cache_keys = "spree/products/all-#{params[:page]}-#{max_updated_at}-#{count}"
       (common_product_cache_keys + [products_cache_keys]).compact.join("/")
