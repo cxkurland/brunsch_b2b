9d711d4310dba23c14845b22243c90db9428a1bf
brijesh varshney <brijesh.varshney@w3villa.com>
2016-11-16 17:06:18 +0530

    modified pagination and toggling of products view

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -5,7 +5,9 @@ module Spree
 	    @searcher = build_searcher(params.merge(include_images: true))
 	    @taxonomies = Spree::Taxonomy.includes(root: :children)
 	    @search = Sunspot.search(Spree::Product) do
-	    	paginate(:page => params[:page] || 1, :per_page => Spree::Config[:products_per_page])
+	    	
+       	 paginate(:page => params[:page] || 1, :per_page => params[:per_page] ? params[:per_page] : 15)	
+	    	# paginate(:page => params[:page] || 1, :per_page => Spree::Config[:products_per_page])
 	    	unless params[:keywords].blank?
 	    		any do
             fulltext "*#{params[:keywords]}*"
