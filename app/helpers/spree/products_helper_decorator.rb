ea540c00a15c837e36b309f17416e9c85acfcff9
Ishank Gupta <ishank_18@yahoo.com>
2016-11-16 18:07:24 +0530

    Fixed issue with cache

--- app/helpers/spree/products_helper_decorator.rb
+++ app/helpers/spree/products_helper_decorator.rb
@@ -7,8 +7,12 @@ module Spree
 
       # max_updated_at = @products.collect(&:updated_at).max.to_s(:number)
       # max_updated_at = (@products.maximum(:updated_at) || Date.today).to_s(:number)
-      products_cache_keys = "spree/products/all-#{params[:page]}-#{max_updated_at}-#{count}"
+      products_cache_keys = "spree/products/all-#{params[:page]}-#{max_updated_at}-#{count}-#{params[:view]}-#{params[:per_page]}"
       (common_product_cache_keys + [products_cache_keys]).compact.join("/")
+    end
+
+    def cache_key_for_product(product = @product)
+      (common_product_cache_keys + [product.cache_key, product.possible_promotions] + [params[:view]]).compact.join("/")
     end
 
   end
